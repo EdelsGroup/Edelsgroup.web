077be9d9d079823d84f02d689b6bfff1262e6661
Esteban Charria Muñoz <charriaesteban9@gmail.com>
2025-05-04 12:43:37 -0500

    remove watermark from CardFlip and Gallery components

--- src/components/CardFlip.jsx
+++ src/components/CardFlip.jsx
@@ -1,4 +1,3 @@
-import { Watermark } from "antd";
 import "../styles/cardFlip.css";
 
 export default function CardFlip({ src, title, text }) {
@@ -6,10 +5,7 @@ export default function CardFlip({ src, title, text }) {
     <div className="flip-card">
       <div className="flip-card-inner">
         <div className="flip-card-front">
-          <Watermark height={30} width={130} content="Edel's Group" font={{color: 'rgba(255,2555,255,0.5)', fontSize: 20, fontFamily: 'Nunito Sans'}}>
-            <img className="flip-card-front-img" src={src} alt={title} />
-          </Watermark>
-          {/* <WatermarkedImage className="flip-card-front-img" src={src} alt={title}/> */}
+          <img className="flip-card-front-img" src={src} alt={title} />
         </div>
         <div className="flip-card-back">
           <p className="flip-card-back-title">{title}</p>

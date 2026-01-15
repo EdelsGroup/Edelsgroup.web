d4805f89adde21768b761bdce2820ef7308e7fc3
Esteban Charria Muñoz <charriaesteban9@gmail.com>
2026-01-14 19:22:48 -0500

    update map coordinates for improved location accuracy

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -4,12 +4,12 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const Map = () => {
   return (
-    <MapContainer center={[3.3900894, -76.5411889]    } zoom={50} scrollWheelZoom={false} style={{ height: '400px', width: '100%' }}>
+    <MapContainer center={[3.3906597, -76.5400865]    } zoom={50} scrollWheelZoom={false} style={{ height: '400px', width: '100%' }}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[3.3900894, -76.5411889]}>
+      <Marker position={[3.3906597, -76.5400865]}>
         <Popup>
           Encuentranos <a target='_blank' href='https://maps.app.goo.gl/9Rk9Bs5C6KQsHujK6'>Aqui</a>
         </Popup>

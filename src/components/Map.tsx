1e80fec2d1479ba3318e700b4dccbc2cd98cea9f
Esteban Charria Muñoz <charriaesteban9@gmail.com>
2026-01-15 19:34:42 -0500

    update map coordinates and popup link for improved accuracy

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -4,14 +4,14 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const Map = () => {
   return (
-    <MapContainer center={[3.3906597, -76.5400865]    } zoom={50} scrollWheelZoom={false} style={{ height: '400px', width: '100%' }}>
+    <MapContainer center={[3.3904816, -76.539794]    } zoom={50} scrollWheelZoom={false} style={{ height: '400px', width: '100%' }}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[3.3906597, -76.5400865]}>
+      <Marker position={[3.3904816, -76.539794]}>
         <Popup>
-          Encuentranos <a target='_blank' href='https://maps.app.goo.gl/9Rk9Bs5C6KQsHujK6'>Aqui</a>
+          Encuentranos <a target='_blank' href='https://maps.app.goo.gl/cX36vi3dQGg2A7Uy8'>Aqui</a>
         </Popup>
       </Marker>
     </MapContainer>
